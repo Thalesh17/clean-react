373b7d179b09af5791e200d623ca2b91f0259692
Henrique Gomes Bezerra <thaalesheenrique@gmail.com>
2021-01-07 20:03:36 -0300

    chore: add jest

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,11 @@
+import { AccountModel } from './models/accountmodel';
+
+type AuthenticationParams = {
+    email: string;
+    password: string
+}
+
+export interface Authentication {
+    auth(params: AuthenticationParams): Promise<AccountModel>
+
+}
