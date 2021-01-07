99df4f8368eb130d80f57383ea3ddfaf933c553d
Henrique Gomes Bezerra <thaalesheenrique@gmail.com>
2021-01-07 20:47:58 -0300

    refactor: move HttpPostClientSpy to its own file

---
+++ src/data/test/mock-http-client.ts
@@ -0,0 +1,8 @@
+export class HttpPostClientSpy implements HttpPostClientSpy {
+    url?: string;
+
+    async post (url: string): Promise<void> {
+        this.url = url;
+        return Promise.resolve();
+    }
+}
